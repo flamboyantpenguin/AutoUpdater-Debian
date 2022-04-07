8cacee2bb4175740a95b6c6108353abd3590ecab
Adithyan N A <49310641+ShapedAna@users.noreply.github.com>
2022-04-07 11:48:38 +0530

    Zenity package checking
    Checks whether zenity is installed and installs it if not installed with user approval.

--- install.sh
+++ install.sh
@@ -4,6 +4,25 @@
 #Run the command ./install.sh or Double Click the file to begin
 #Note : You may need to enable execute permissions to execute this file
 
+command -v zenity
+if [ $? = 1 ]; then
+  echo "zenity is not installed. Do you want to install zenity? (Y/N) "
+  read opt
+  if [ $opt = "Y"]; then
+    echo "Installing zenity using apt"
+    sudo apt install zenity -y 
+    if [ $? = 1 ]; then
+      echo "Installation of zenity failed, cannot start installer without zenity"
+      echo "Exiting installer"
+      exit 1
+    else 
+      echo "Installation successfull, resuming AutoUpdater installation"
+    fi
+  else 
+    echo "Cannot start installer without zenity"
+    echo "Exiting installer"
+    exit 1
+  fi
 zenity --info --text="Welcome to AutoUpdater6.8 PUBLIC BETA Installer"
 opt=$(zenity --entry --text "Enter old for older versions of Ubuntu and other for others" --title="AutoUpdater6.8 BETA Installer")
 if [ $opt = "old" ]; then
@@ -16,4 +35,5 @@ else
   echo -n $password | sudo -S tar -xvf files.tar
   sudo sh ./installer
   zenity --info --text="Installation Completed Successfully"
+  exit 0
 fi
